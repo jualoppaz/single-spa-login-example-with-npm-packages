74e96e4bad8973c66efe01df7840a74b10a79035
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-05 19:31:17 +0100

    Fixes eslint warnings and register vue-app application

--- root-application.js
+++ root-application.js
@@ -1,38 +1,47 @@
-import * as singleSpa from "single-spa";
+/* eslint-disable func-names */
+import * as singleSpa from 'single-spa';
 import 'zone.js';
 
-singleSpa.registerApplication(
-  "login",
-  () => import("single-spa-auth-app"),
-  showWhenAnyOf(["/login"])
-);
-singleSpa.registerApplication(
-  "layout",
-  () => import("single-spa-layout-app"),
-  showExcept(["/login"])
-);
-singleSpa.registerApplication(
-    "angular",
-    () => import("single-spa-angular-app"),
-    showWhenPrefix(["/angular"])
-  );
-
-singleSpa.start();
-
 function showWhenAnyOf(routes) {
-  return function(location) {
-    return routes.some(route => location.pathname === route);
+  return function (location) {
+    return routes.some((route) => location.pathname === route);
   };
 }
 
 function showWhenPrefix(routes) {
-  return function(location) {
-    return routes.some(route => location.pathname.startsWith(route));
+  return function (location) {
+    return routes.some((route) => location.pathname.startsWith(route));
   };
 }
 
 function showExcept(routes) {
-  return function(location) {
-    return routes.every(route => location.pathname !== route);
+  return function (location) {
+    return routes.every((route) => location.pathname !== route);
   };
 }
+
+singleSpa.registerApplication(
+  'login',
+  () => import('single-spa-auth-app'),
+  showWhenAnyOf(['/login']),
+);
+
+singleSpa.registerApplication(
+  'layout',
+  () => import('single-spa-layout-app'),
+  showExcept(['/login']),
+);
+
+singleSpa.registerApplication(
+  'angular',
+  () => import('single-spa-angular-app'),
+  showWhenPrefix(['/angular']),
+);
+
+singleSpa.registerApplication(
+  'vue',
+  () => import('single-spa-vue-app'),
+  showWhenPrefix(['/vue']),
+);
+
+singleSpa.start();
